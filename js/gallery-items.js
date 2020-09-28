1221b57a00487905c825ec0d1660424c18978029
Ihor Nadiein <63648254+ingvar1802@users.noreply.github.com>
2020-09-28 17:24:31 +0300

    + модалка

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,4 +1,3 @@
-"use strict";
 const images = [
   {
     preview:
@@ -66,13 +65,18 @@ const images = [
 ];
 
 const galleryContainer = document.querySelector(".js-gallery");
-const cardsMarkup = createGalleryMarkup(images);
+const modal = document.querySelector(".js-lightbox");
+const modalContent = document.querySelector(".lightbox__content");
+const modalImage = document.querySelector(".lightbox__image");
+const modalBtn = document.querySelector(".lightbox__button");
 
-galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+galleryContainer.addEventListener("click", openModal);
+modalBtn.addEventListener("click", closeModal);
+galleryContainer.insertAdjacentHTML("beforeend", createGalleryMarkup(images));
 
 function createGalleryMarkup(images) {
   return images
-    .map(({ original, preview }) => {
+    .map(({ original, preview, description }) => {
       return `
       <li class="gallery__item">
         <a
@@ -83,11 +87,30 @@ function createGalleryMarkup(images) {
             class="gallery__image"
             src="${preview}"
             data-source="${original}"
-            alt="Tulips"
+            alt="${description}"
           />
         </a>
       </li>
         `;
     })
     .join("");
+}
+
+function openModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  document.addEventListener("keydown", closeModal);
+
+  modal.classList.add("is-open");
+  modalImage.src = event.target.dataset.source;
+  modalImage.alt = event.target.alt;
+}
+
+function closeModal(event) {
+  event.preventDefault();
+  document.removeEventListener("keydown", closeModal);
+  modal.classList.remove("is-open");
+  modalImage.src = "";
 }
